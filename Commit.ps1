07db1138ff867b1347e75e72c66dc8086a48ca78
Masakazu Kayano <masakazu.kayano@gmail.com>
2020-05-31 18:22:04 +0900

    Update Commmit.ps1 Commited at 2020/05/31 18:21 (JST)

--- Commit.ps1
+++ Commit.ps1
@@ -1,5 +1,5 @@
 
-Param($Reson)
+
 $DateTime = (Get-Date -Format "yyyy/MM/dd HH:mm") + " (JST)"
 
 cscript vbac.wsf decombine
@@ -8,7 +8,7 @@ git add ./*.*
 git add ./bin/*.*
 git add ./src
 
-git commit -m "$Reson Commited at $DateTime"
+git commit -m "$args Commited at $DateTime"
 
 git push origin master
 git push origin
