d4a77d330f11a317dfb309633f4df35452852566
Masakazu Kayano <masakazu.kayano@gmail.com>
2020-05-31 18:35:57 +0900

    Commit.ps1 add echo Commited at 2020/05/31 18:35 (JST)

--- Commit.ps1
+++ Commit.ps1
@@ -2,13 +2,16 @@
 
 $DateTime = (Get-Date -Format "yyyy/MM/dd HH:mm") + " (JST)"
 
+echo "cscript vbac.wsf decombine`n"
 cscript vbac.wsf decombine
 
-git add ./*.*
-git add ./bin/*.*
-git add ./src
+echo "git add`n"
+git add .
 
+echo "git commit`n"
 git commit -m "$args Commited at $DateTime"
 
+echo "git push master`n"
 git push origin master
+echo "git push origin`n"
 git push origin
